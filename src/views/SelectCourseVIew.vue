442a51a15ca2d470f9f0cba207ce8eaa7db17dfd
randomCui <czy_scu@qq.com>
2023-04-24 14:09:37 +0800

    针对tauri进行迁移适配工作

--- src/views/SelectCourseVIew.vue
+++ src/views/SelectCourseVIew.vue
@@ -17,6 +17,7 @@
         v-if="search_mode === '复杂搜索'"
         label-position="left"
         label-width="5em"
+        v-loading="isLoading"
     >
       <el-row justify="space-around">
         <el-col :span="12">
@@ -33,8 +34,8 @@
         <el-col style="display: flex;align-content: center" :span="8">
           <el-form-item style="justify-self: right">
             <el-button-group>
-              <el-button style="width: 7em" @click="sendSearchFilterToIPC">搜索</el-button>
-              <el-button style="width: 7em" @click="altSearch('full')">非正选搜索</el-button>
+              <el-button style="width: 7em" @click="search('full', 'formal')">搜索</el-button>
+              <el-button style="width: 7em" @click="search('full', 'alt')">非正选搜索</el-button>
             </el-button-group>
           </el-form-item>
         </el-col>
@@ -46,6 +47,7 @@
         v-else-if="search_mode === '简单搜索'"
         label-position="left"
         label-width="5em"
+        v-loading="isLoading"
     >
       <el-row justify="space-around">
         <el-col :span="12">
@@ -56,8 +58,8 @@
         <el-col :span="8">
           <el-form-item>
             <el-button-group>
-              <el-button @click="sendSearchFilterToIPC">搜索</el-button>
-              <el-button @click="altSearch('simple')">非正选搜索</el-button>
+              <el-button @click="search('simple', 'formal')">搜索</el-button>
+              <el-button @click="search('simple', 'alt')">非正选搜索</el-button>
             </el-button-group>
           </el-form-item>
         </el-col>
@@ -180,11 +182,13 @@
 </template>
 
 <script>
-import {fetch} from '@tauri-apps/api/http';
-import {course_select_search_url} from '../config/endPoint.js';
-import {useCounterStore} from '../store/selectCourseStore'
+import {fetch, Body} from '@tauri-apps/api/http';
+import {course_select_search_url, http_head} from '../config/endPoint.js';
 import {DesiredCourse} from "../js/selectCourse.js";
-import {useSchedulerStore} from "../store/courseSchedulerCoures.js";
+import {useCourseStore} from "../store/courseStore.js";
+import CourseQuery from "../js/queryCourse.js";
+import config from "../config/webSessionEssential.js";
+
 export default {
   name: "SelectCourseVIew",
   data() {
@@ -203,146 +207,97 @@ export default {
         3: "三",
         4: "四",
         5: "五"
-      }
-    };
+      },
+      queryWorker: new CourseQuery(),
+      isLoading: false,
+    }
   },
   mounted() {
-    //   window.ipc.invoke("get_course_list_cached").then(res => {
-    //     return JSON.parse(res);
-    //   }).then(json => {
-    //     this.courses = json.sort((a, b) => {
-    //       if (+a.number - +b.number === 0) {
-    //         return +a.seqNumber - +b.seqNumber;
-    //       } else {
-    //         return +a.number - +b.number;
-    //       }
-    //     });
-    //   });
-    //   window.ipc.invoke("modify_selection_list", JSON.stringify({
-    //       op: "get cache"
-    //     })
-    //   ).then(res => {
-    //     return JSON.parse(res);
-    //   }).then(json => {
-    //     this.course_selected = new Set(json);
-    //   });
-    const courseStore = useCounterStore();
-    this.courses = courseStore.list;
+    const store = useCourseStore();
+    this.courses = store.searchCacheList;
+    this.course_selected = store.scheduledSet;
   },
   methods: {
-    sendSearchFilterToIPC() {
-      if (this.search_mode === "简单搜索") {
+    async search(search_mode, channel) {
+      if (
+          (this.search_filter.name === undefined || this.search_filter.name === "") &&
+          (this.search_filter.teacher === undefined || this.search_filter.teacher === "") &&
+          (this.search_filter.number === undefined || this.search_filter.number === "")
+      ) {
+        ElMessage.error('搜索条件不能为空')
+        return;
+      }
+      this.isLoading = true;
+      switch (channel) {
+        case "formal":
+          await this.searchFormal(search_mode);
+          this.isLoading = false;
+          break;
+        case "alt":
+          await this.altSearch(search_mode);
+          this.isLoading = false;
+          break;
+      }
+    },
+    async searchFormal(search_mode) {
+      if (search_mode === "simple") {
         let simple_filter = JSON.parse(JSON.stringify(this.search_filter))
         simple_filter.teacher = "";
         simple_filter.number = "";
-        fetch(course_select_search_url,
-            {
-              method: "GET",
-            }
-        ).then(
-            res => {
-              console.log(res.data)
-              const courseStore = useCounterStore();
-              let templist = [];
-              for (let course of res.data.rwRxkZlList){
-                templist.push(new DesiredCourse(course))
-              }
-              courseStore.list = templist;
-              this.courses = templist.sort((a, b) => {
-                    if (+a.number - +b.number === 0) {
-                      return +a.seqNumber - +b.seqNumber;
-                    } else {
-                      return +a.number - +b.number;
-                    }
-                  });
-            }
+        await this.queryWorker.searchCourse(simple_filter).then(
+            courseList => this.sync_search_result_to_local(courseList)
         )
-
-        // window.ipc.invoke("get_course_list", JSON.stringify(simple_filter)
-        // ).then(res => {
-        //   return JSON.parse(res);
-        // }).then(json => {
-        //   this.courses = json.sort((a, b) => {
-        //     if (+a.number - +b.number === 0) {
-        //       return +a.seqNumber - +b.seqNumber;
-        //     } else {
-        //       return +a.number - +b.number;
-        //     }
-        //   });
-        // });
       } else {
-        // window.ipc.invoke("get_course_list", JSON.stringify(this.search_filter)).then(res => {
-        //   return JSON.parse(res);
-        // }).then(json => {
-        //   this.courses = json.sort((a, b) => {
-        //     if (+a.number - +b.number === 0) {
-        //       return +a.seqNumber - +b.seqNumber;
-        //     } else {
-        //       return +a.number - +b.number;
-        //     }
-        //   });
-        // });
+        await this.queryWorker.searchCourse(this.search_filter).then(
+            courseList => this.sync_search_result_to_local(courseList)
+        )
       }
     },
 
-    altSearch(search_mode) {
-      if (search_mode === "简单搜索") {
+    async altSearch(search_mode) {
+      if (search_mode === "simple") {
         let simple_filter = JSON.parse(JSON.stringify(this.search_filter))
         simple_filter.teacher = "";
         simple_filter.number = "";
-        // window.ipc.invoke("get_course_list_alt", JSON.stringify(simple_filter)
-        // ).then(res => {
-        //   return JSON.parse(res);
-        // }).then(json => {
-        //   this.courses = json.sort((a, b) => {
-        //     if (+a.number - +b.number === 0) {
-        //       return +a.seqNumber - +b.seqNumber;
-        //     } else {
-        //       return +a.number - +b.number;
-        //     }
-        //   });
-        // });
+        await this.queryWorker.searchCourseAlt(simple_filter).then(
+            courseList => this.sync_search_result_to_local(courseList)
+        )
       } else {
-        // window.ipc.invoke("get_course_list_alt", JSON.stringify(this.search_filter)).then(res => {
-        //   return JSON.parse(res);
-        // }).then(json => {
-        //   console.log(json);
-        //   this.courses = json.sort((a, b) => {
-        //     if (+a.number - +b.number === 0) {
-        //       return +a.seqNumber - +b.seqNumber;
-        //     } else {
-        //       return +a.number - +b.number;
-        //     }
-        //   });
-        // });
+        await this.queryWorker.searchCourseAlt(this.search_filter).then(
+            courseList => this.sync_search_result_to_local(courseList)
+        )
       }
     },
 
     toggleSelection(uid) {
-      const ss = useSchedulerStore()
+      const store = useCourseStore()
       if (this.course_selected.has(uid)) {
         this.course_selected.delete(uid);
-        ss.scheduledList.splice(
-            this.courses.findIndex(value => {return value.ID === uid})[0],
-            1
-        )
-        // window.ipc.invoke("modify_selection_list", JSON.stringify({
-        //   op: "rm",
-        //   course_ID: uid
-        // }));
+        store.removeCourseFromSchedule(uid);
       } else {
         this.course_selected.add(uid);
-        ss.scheduledList.push(this.courses.filter(value => {return value.ID === uid})[0])
-        // window.ipc.invoke("modify_selection_list", JSON.stringify({
-        //   op: "add",
-        //   course_ID: uid
-        // }));
+        store.addCourseToSchedule(this.courses.filter(value => {
+          return value.ID === uid
+        })[0]);
       }
-      console.log(this.course_selected);
-      // window.ipc.invoke("modify_selection_list")
+      this.course_selected = store.scheduledSet;
+    },
+
+    sync_search_result_to_local(courseList) {
+      this.courses = courseList.sort((a, b) => {
+        if (+a.number - +b.number === 0) {
+          return +a.seqNumber - +b.seqNumber;
+        } else {
+          return +a.number - +b.number;
+        }
+      });
+      const store = useCourseStore();
+      store.searchCacheList = this.courses;
+      this.course_selected = store.scheduledSet;
     }
   },
 };
+
 </script>
 
 <style scoped>
