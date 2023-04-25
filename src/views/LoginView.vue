b694f2df4704ab05fe93c501d6c203d3a6df899d
randomCui <czy_scu@qq.com>
2023-04-25 11:40:52 +0800

    feat: 实现了教务登录的相关功能

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,101 +1,110 @@
 <template>
-  <el-card v-if="!isLogin" class="box-card">
+  <el-card v-if="!stateStore.isLogin" class="box-card">
     <template #header>
       <div class="card-header">
         <span>登录教务</span>
-<!--        <el-button class="button" text>Operation button</el-button>-->
+        <!--        <el-button class="button" text>Operation button</el-button>-->
       </div>
     </template>
     <el-form :model="loginInfo" label-width="120px">
       <el-form-item label="学号">
-        <el-input v-model="loginInfo.studentID" clearable />
+        <el-input v-model="loginInfo.studentID" clearable/>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input type="password" v-model="loginInfo.password" clearable show-password />
+        <el-input type="password" v-model="loginInfo.password" clearable show-password/>
       </el-form-item>
       <el-form-item label="验证码">
         <el-input v-model="loginInfo.captcha" clearable>
           <template #append>
-            <el-image :src="captchaUrl" @click="refreshCaptcha" style="height:2em;width:6em" />
+            <el-image :src="captchaUrl" @click="refreshCaptcha" style="height:2em;width:6em"/>
           </template>
         </el-input>
       </el-form-item>
-        <el-button type="primary" @click="onLoginButtonPressed">登录</el-button>
+      <el-button type="primary" @click="onLoginButtonPressed">登录</el-button>
     </el-form>
   </el-card>
   <el-text type="success" v-else>
     已登录
   </el-text>
-<!--  <div style="display: flex;justify-content: center;align-content: center">-->
-<!--    <div v-if="!isLogin" class="login-plate">-->
-<!--      <h3>账号登陆</h3>-->
-<!--      <form class="login-wrapper" style="display: grid">-->
-<!--        <input v-model="studentID" class="account-input" placeholder="学号" type="text" maxlength="13">-->
-<!--        <input v-model="password" class="password-input" placeholder="密码" type="password">-->
-<!--        <div style="display: flex; flex-direction: row; justify-content: center">-->
-<!--          <input v-model="captcha" class="captcha-input" placeholder="验证码" type="text" maxlength="4">-->
-<!--          <img alt="Captcha image" :src="captchaUrl" @click="refreshCaptcha">-->
-<!--        </div>-->
-<!--        <div class="login-btn" @click="onLoginButtonPressed">-->
-<!--          <div>-->
-<!--            登录-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </form>-->
-<!--    </div>-->
-<!--    <div v-else>-->
-<!--      <h2>已经登陆过了</h2>-->
-<!--    </div>-->
-<!--  </div>-->
+  <!--  <div style="display: flex;justify-content: center;align-content: center">-->
+  <!--    <div v-if="!isLogin" class="login-plate">-->
+  <!--      <h3>账号登陆</h3>-->
+  <!--      <form class="login-wrapper" style="display: grid">-->
+  <!--        <input v-model="studentID" class="account-input" placeholder="学号" type="text" maxlength="13">-->
+  <!--        <input v-model="password" class="password-input" placeholder="密码" type="password">-->
+  <!--        <div style="display: flex; flex-direction: row; justify-content: center">-->
+  <!--          <input v-model="captcha" class="captcha-input" placeholder="验证码" type="text" maxlength="4">-->
+  <!--          <img alt="Captcha image" :src="captchaUrl" @click="refreshCaptcha">-->
+  <!--        </div>-->
+  <!--        <div class="login-btn" @click="onLoginButtonPressed">-->
+  <!--          <div>-->
+  <!--            登录-->
+  <!--          </div>-->
+  <!--        </div>-->
+  <!--      </form>-->
+  <!--    </div>-->
+  <!--    <div v-else>-->
+  <!--      <h2>已经登陆过了</h2>-->
+  <!--    </div>-->
+  <!--  </div>-->
 </template>
 
+<script setup>
+import {useStateStore} from "../store/urpStateStore.js";
+
+const stateStore = useStateStore();
+</script>
 <script>
+import {init_urp_login, login_info_file, post_login_info, refresh_captcha} from "../js/urpLogin";
+
 export default {
   name: "LoginView",
   data() {
     return {
       captchaUrl: undefined,
-      loginInfo:{
+      loginInfo: {
         studentID: "",
         password: "",
         captcha: "",
       },
-      isLogin: window.localStorage.getItem("isLogin") === "true" || true
+      isLogin: false,
     };
   },
   mounted() {
-    /* eslint-disable*/
-    this.isLogin = window.localStorage.getItem("isLogin") === "true" || true;
-    // window.ipc.invoke("urp_login_state").then(state => {
-    //   console.log(JSON.parse(state));
-    //   this.isLogin = JSON.parse(state);
-    //   if (JSON.parse(state)) {
-    //     window.localStorage.setItem("isLogin", "true");
-    //   } else {
-    //     window.localStorage.setItem("isLogin", "false");
-    //   }
-    //   return JSON.parse(state);
-    // }).then((isLogin) => {
-    //   if (!isLogin) {
-    //     window.ipc.invoke("init_urp_login").then(res => {
-    //       this.captchaUrl = URL.createObjectURL(new Blob([res], { type: "image/jpeg" }));
-    //     });
-    //     window.ipc.invoke("login_info_file",JSON.stringify({
-    //       op: "read"
-    //     })).then(res=>{
-    //       res = JSON.parse(res)
-    //       if(res.status === "success") {
-    //         this.loginInfo.password = res.password
-    //         this.loginInfo.studentID = res.studentID
-    //       }
-    //     })
-    //   }
-    // });
-
+    const stateStore = useStateStore();
+    this.isLogin = stateStore.isLogin;
+    init_urp_login().then(data => {
+      // console.log(data)
+      this.captchaUrl = URL.createObjectURL(new Blob([Uint8Array.from(data)], {type: "image/jpeg"}));
+    });
+    login_info_file({op: 'read'}).then(res => {
+      if (res.status === "success") {
+        this.loginInfo.password = res.password
+        this.loginInfo.studentID = res.studentID
+      }
+    })
   },
   methods: {
     onLoginButtonPressed() {
-      // console.log(this.studentID,this.password,this.captcha)
+      console.log(this.loginInfo);
+      post_login_info({
+        student_id: this.loginInfo.studentID,
+        password: this.loginInfo.password,
+        captcha: this.loginInfo.captcha
+      }).then(res => {
+            console.log(res);
+            if (res.status === 'success') {
+              ElMessage.success(res.message);
+              state.isLogin = true
+            } else if (res.status === "failed") {
+              ElMessage.error(res.message);
+              this.isLogin = false
+              init_urp_login().then(res => {
+                this.captchaUrl = URL.createObjectURL(new Blob([Uint8Array.from(res)], {type: "image/jpeg"}));
+              });
+            }
+          }
+      )
       // window.ipc.invoke("post_login_info", JSON.stringify({
       //   student_id: this.loginInfo.studentID,
       //   password: this.loginInfo.password,
@@ -123,9 +132,9 @@ export default {
       // });
     },
     async refreshCaptcha() {
-      // window.ipc.invoke("refresh_captcha").then(data => {
-      //   this.captchaUrl = URL.createObjectURL(new Blob([data], { type: "image/jpeg" }));
-      // });
+      refresh_captcha().then(data => {
+        this.captchaUrl = URL.createObjectURL(new Blob([Uint8Array.from(data)], {type: "image/jpeg"}));
+      });
     },
   }
 };
