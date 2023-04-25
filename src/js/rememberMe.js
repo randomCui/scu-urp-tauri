341fc948607d65610db2d3e90162e2fdbea51140
randomCui <czy_scu@qq.com>
2023-04-25 11:40:27 +0800

    feat: 将保存账号密码的功能从node的fs技术栈迁移至tauri的fs技术栈

--- src/js/rememberMe.js
+++ src/js/rememberMe.js
@@ -1,37 +1,44 @@
-import fs from "fs"
-export function saveLoginInfo(studentID, password) {
-  let config = {}
-  if (fs.existsSync('./config.json')) {
-    let buffer = fs.readFileSync('./config.json');
-    config = JSON.parse(buffer);
-  }
-  config['loginInfo'] = {
-    'studentID': studentID,
-    'password': password,
-  }
-  // console.log(process.cwd());
-  fs.writeFile('./config.json', JSON.stringify(config), () => {
-  });
-}
+// import fs from "fs"
+import {readTextFile, writeTextFile, exists} from "@tauri-apps/api/fs";
+import {resourceDir, join, BaseDirectory} from '@tauri-apps/api/path';
 
-export function readLoginInfo() {
-  if (fs.existsSync('./config.json')) {
-    let buffer = fs.readFileSync('./config.json');
-    let config = JSON.parse(buffer);
-    let studentID = config['loginInfo']['studentID'];
-    let password = config['loginInfo']['password'];
-    if (studentID && password)
-      return {
-        'status': 'success',
+export async function saveLoginInfo(studentID, password) {
+    const resourceDirPath = await resourceDir();
+    const configFilePath = await join(resourceDirPath, "config.json")
+    let config = {}
+    if (await exists(configFilePath)) {
+        let buffer = await readTextFile(configFilePath);
+        config = JSON.parse(buffer);
+    }
+    config['loginInfo'] = {
         'studentID': studentID,
         'password': password,
-      }
-    else
-      return {
-        'status': 'empty'
-      }
-  } else
-    return {
-      'status': 'not exist'
     }
+    // console.log(JSON.stringify(config));
+    await writeTextFile({path: 'config.json', contents: JSON.stringify(config)}, {dir: BaseDirectory.Resource});
+}
+
+export async function readLoginInfo() {
+    const resourceDirPath = await resourceDir();
+    const configFilePath = await join(resourceDirPath, "config.json")
+    if (await exists(configFilePath)) {
+        let buffer = await readTextFile(configFilePath);
+        // console.log(buffer)
+        let config = JSON.parse(buffer);
+        let studentID = config['loginInfo']['studentID'];
+        let password = config['loginInfo']['password'];
+        if (studentID && password)
+            return {
+                'status': 'success',
+                'studentID': studentID,
+                'password': password,
+            }
+        else
+            return {
+                'status': 'empty'
+            }
+    } else
+        return {
+            'status': 'not exist'
+        }
 }
